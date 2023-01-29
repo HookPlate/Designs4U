6ac0fe0645ec79c279ae60c2d41be7fd8c1ea5e3
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-01-29 08:55:17 +0000

    first commit gets rid of comments from last branch

--- Designs4U/ContentView.swift
+++ Designs4U/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
         NavigationStack {
             ScrollView {
                 LazyVStack {
-                    //use only the ones who ,m,atch our search text.
                     ForEach(model.searchResults) { person in
                         DesignerRow(person: person, model: model)
                     }
@@ -23,7 +22,6 @@ struct ContentView: View {
                 .padding(.horizontal)
             }
             .navigationTitle("Designs4u")
-            //we get basic searching up and running by adding a single modifier to our model, this gives us the search bar below the nav title.
             .searchable(text: $model.searchText)
         }
         .task {
--- Designs4U/DataModel.swift
+++ Designs4U/DataModel.swift
@@ -11,21 +11,19 @@ import SwiftUI
 @MainActor
 class DataModel: ObservableObject{
     @Published var people = [Person]()
-    //stores some search text typed into the screen
+
     @Published var searchText = ""
-    //A subset of the people array based on what matches their queiry
+
     var searchResults: [Person] {
         people.filter { person in
-            //they type nothing, this person is definately in the final array because there's no text to match on.
             guard searchText.isEmpty == false else { return true }
-            //if we're still here it means they've typed some search text, which of those fields match that search text if any?
-            //if any of those strings match (case insensitively) our searchText, return true.
+            
             for string in [person.firstName, person.lastName, person.bio, person.details] {
                 if string.localizedCaseInsensitiveContains(searchText) {
                     return true
                 }
             }
-            //if we get to here, non of them matched, do not include this person in the final results
+            
             return false
         }
     }
