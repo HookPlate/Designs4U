5e862671cfc144846bf28ce25bfd200bc370997f
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-01-30 21:17:51 +0000

    Adds the some scrolling text views at the bottom of the presents sheet (that uses presentationDetents so it's half a sheet. Makes it obvious the HSTack win those texts is scrollable with a mask

--- DesignerDetails.swift
+++ DesignerDetails.swift
@@ -28,14 +28,42 @@ struct DesignerDetails: View {
                 VStack(alignment: .leading, spacing: 10) {
                     Text(person.displayName)
                         .font(.largeTitle.bold())
+                        //new in IOS 16 doesn't work here..fontDesign(.rounded)
                     Text(person.bio)
                     Text(person.details)
                 }
                 //this VStack has the same setting os the last but it has padding, meaning the bio and details will have padding but not the mother VStack, see simulator.
                 .padding()
+                //At the bottom of the mother VStack, indicators are the little gray lines that give the users a sense of how long the scroll content is.
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(person.tags, id: \.self) { tag in
+                            Text(tag)
+                                .padding(5)
+                          //  extra padding horizontally so it looks better on the screen
+                                .padding(.horizontal)
+                            //new in IOS 16 is the .gradient color variation
+                                .background(.blue.gradient)
+                                .foregroundColor(.white)
+                                .clipShape(Capsule())
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                //gives a slight faded effect to one edge of the HStack showing the user that the view is scrollable
+                .mask(
+                    LinearGradient(stops: [
+                        .init(color: .clear, location: 0),
+                        .init(color: .white, location: 0.05),
+                        .init(color: .white, location: 0.95),
+                        .init(color: .clear, location: 1)
+                    ], startPoint: .leading, endPoint: .trailing)
+                )
             }
             .padding(.vertical)
         }
+        //allows us to present the sheet as a bottom sheet. Since we asked for medium and large there's a drag bar for the user to drag up with the sheet.
+        .presentationDetents([.medium, .large])
     }
 }
 
